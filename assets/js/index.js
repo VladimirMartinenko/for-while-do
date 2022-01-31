cd22f4cb749cacbcd922da8918c2c42f0e28f2e5
Vladimir Martinenko <teslabes87@gmail.com>
2022-01-31 20:47:20 +0200

    add index.js

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+ /*for(let counter = 25;counter >= 0;counter--){
+  console.log(counter);
+};*/
+
+/*for(let number=10;number<=50;number+=5) {
+  console.log(number);
+};*/
+
+/*function summ (number2) {*/
+  for(let number=1,num=0;number<=100;number+++(number++)){
+        console.log(number);
+  }
